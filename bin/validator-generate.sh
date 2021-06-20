7dde7d2b2155bf227cc752ba68d3ff180a33a3af
daheige <zhuwei313@hotmail.com>
2021-06-20 16:35:09 +0800

    validator-gen tools

--- bin/validator-generate.sh
+++ bin/validator-generate.sh
@@ -4,10 +4,19 @@ root_dir=$(cd "$(dirname "$0")"; cd ..; pwd)
 # grep all request validator
 sh $root_dir/bin/validator_grep.sh
 
-# request validator code
-go build -o $root_dir/tools/validator_gen/validator_gen $root_dir/tools/validator_gen/validator.go
+validatorGenExec=$(which "validator_gen")
+if [ -z $validatorGenExec ]; then
+  # request validator code
+  go build -o $root_dir/tools/validator_gen/validator_gen $root_dir/tools/validator_gen/validator.go
+  chmod +x $root_dir/tools/validator_gen/validator_gen
 
-$root_dir/tools/validator_gen/validator_gen -pb_dir=$root_dir/pb -validator_log_dir=$root_dir
+  # copy this binary file to $GOPATH/bin
+  cp $root_dir/tools/validator_gen/validator_gen $GOPATH/bin/validator_gen
+
+  validatorGenExec=$root_dir/tools/validator_gen/validator_gen
+fi
+
+$validatorGenExec -pb_dir=$root_dir/pb -validator_log_dir=$root_dir
 
 echo "generate validator request interceptor success"
 
